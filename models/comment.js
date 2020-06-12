19d937f48a142b35efacf2833b399b27c368a7ba
swaraj-jain <happy2000jain@gmail.com>
2020-06-12 19:02:33 +0530

    real date added

--- models/comment.js
+++ models/comment.js
@@ -2,6 +2,10 @@ var mongoose= require("mongoose");
 
 var commentSchema= mongoose.Schema({
     text:String,
+    time1:{
+        type:String,
+        default:new Date().toISOString().slice(0,10)
+    },
     author:{
         id:{
             type:mongoose.Schema.Types.ObjectId,
